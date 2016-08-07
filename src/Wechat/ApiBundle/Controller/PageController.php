93407535a963a3d361548455fde0e266251ebc5a
Dirc Wang <dirk.wang@samesamechina.com>
2016-08-08 01:15:31 +0800

    add group news2

--- src/Wechat/ApiBundle/Controller/PageController.php
+++ src/Wechat/ApiBundle/Controller/PageController.php
@@ -24,4 +24,20 @@ class PageController extends Controller
   public function replyAction(){
     return $this->render('WechatApiBundle:Page:replay.html.twig');
   }
+
+  public function groupnewsAction(){
+    $wehcat = $this->container->get('my.Wechat');
+    $search = array(
+      'type' => 'news',
+      'offset' => '0',
+      'count' => '10',
+    );
+    $check = $wehcat->getMateriallist($search);
+    if(isset($check['errcode'])){
+      $data = array();
+    }else{
+      $data = $check['item'];
+    }
+    return $this->render('WechatApiBundle:Page:groupnews.html.twig', array('newslist' => $data));
+  }
 }
