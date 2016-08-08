7830e7686384d154b0f077f8a2ad15a17bf868ac
Dirc Wang <dirk.wang@samesamechina.com>
2016-08-08 23:52:15 +0800

    update groupnews send10

--- src/Wechat/ApiBundle/Modals/classes/WechatResponse.php
+++ src/Wechat/ApiBundle/Modals/classes/WechatResponse.php
@@ -203,7 +203,7 @@ class WechatResponse{
         ),
         "msgtype" => "mpnews"
       );
-      $result = $wehcat->sendTagMsg($data);
+      $result = $wehcat->sendTagMsg(json_encode($data, JSON_UNESCAPED_UNICODE));
       $dataSql = $this->container->get('my.dataSql');
       $dataSql->tempEventLog($this->fromUsername, $temp['tempid'],'sendTagMsg',json_encode($result));
       $redis->delkey('wechattemplistener');
