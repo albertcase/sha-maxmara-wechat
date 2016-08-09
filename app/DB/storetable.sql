3fb9c59873411f879ab5660179b0b307d05e6613
Dirc Wang <dirk.wang@samesamechina.com>
2016-08-09 11:06:05 +0800

    add store

---
+++ app/DB/storetable.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `stores`;
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `storename` varchar(100) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `lat` varchar(30) DEFAULT NULL,
+  `lng` varchar(30) DEFAULT NULL,
+  `openhours` varchar(50) NOT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
